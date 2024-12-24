8892a3ad9a3396411eaf1e3c35ebc8851e0c1560
Anatoly <anatoly@siptrinity.com>
2024-12-24 18:31:32 +0200

    one more attempt

---
+++ start-apache.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create necessary Laravel directories and set permissions
+mkdir -p /var/www/html/bootstrap/cache /var/www/html/storage
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
+# Start supervisord to manage apache2
+exec supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
