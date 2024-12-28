ca47f7bf16c3f7dc9aa3ab63874385ef0dec1892
AnatolyZZZ <anatoly.zykov@gmail.com>
2024-12-28 13:36:33 +0200

    maybe

--- laravel-entrypoint.sh
+++ laravel-entrypoint.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-# first copy files from host -> backup -> varr/www/html
-cp -rv /var/www/backup/ /var/www/html/
-
+chown -R www-data:www-data /var/www/html
+chmod -R 775 /var/www/html
 
 # Perform Composer install
 composer install --optimize-autoloader
 
 curl -o wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
 chmod +x wait-for-it.sh
-
-# Set correct permissions for storage and cache directories
-mkdir -p /var/www/html/bootstrap/cache /var/www/html/storage
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
-
 
 # Wait for MySQL to be ready using wait-for-it script
 ./wait-for-it.sh mysql:3306 --timeout=30 --strict -- echo "MySQL is ready"
--- start-apache.sh
+++ start-apache.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Create necessary Laravel directories and set permissions
-mkdir -p /var/www/html/bootstrap/cache /var/www/html/storage /var/www/html/apache_log 
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/apache_log 
-chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/apache_log 
-
 # Start supervisord to manage apache2
 exec supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
