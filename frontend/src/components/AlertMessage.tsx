76cc19050914ed4e50ac4798ad57bc8c6141dd7a
AnatolyZZZ <anatoly.zykov@gmail.com>
2024-12-31 12:04:07 +0200

    alert finished

--- frontend/src/components/AlertMessage.tsx
+++ frontend/src/components/AlertMessage.tsx
@@ -1,14 +1,26 @@
-import Alert from '@mui/material/Alert';
-import { useSelector, shallowEqual } from 'react-redux';
+import {Alert} from '@mui/material';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import {  RootState } from '@store/store';
+import { AlertType } from "@store/types.d";
+import {setAlertShow} from '@store/actions/app'
 
 import './styles/AlertMessage.scss'
 
-const AlertMessage: React.FC = () => {
-	const {  show_alert, alert_message , alert_type } = useSelector<RootState, RootState['app']>(state => state.app, shallowEqual);
+const getVariant = (type: AlertType) => type === 'error' ? 'filled' : 'standard'
 
+const AlertMessage: React.FC = () => {
+	const dispatch = useDispatch()
+	const {  show_alert, alert_message , alert_type } = useSelector<RootState, RootState['app']>(state => state.app, shallowEqual);
 	const classes = show_alert ? "layout-alert-wrapp layout-alert-wrapp_show" : "layout-alert-wrapp "
-	return <div className={classes}><Alert severity={ alert_type }>{ alert_message }</Alert></div>
+	return  <div className={classes}>
+				<Alert 
+					severity={ alert_type }
+					variant={getVariant(alert_type)}
+					onClose={() => dispatch(setAlertShow(false)) }
+				>
+					{ alert_message }
+				</Alert>
+			</div>
 }
 
 export default AlertMessage
