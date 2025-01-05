9bfd38d58aecbdbb30fd9d871223c09d93322717
Anatoly <anatoly@siptrinity.com>
2025-01-05 17:10:24 +0200

    ssl and start-apache  upd

--- start-apache.sh
+++ start-apache.sh
@@ -2,17 +2,18 @@
 
 # Configure Apache
 # Enable the Apache modules
-a2enmod proxy
-a2enmod rewrite
-a2enmod headers
+ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/
+ln -s /etc/apache2/mods-available/headers.load /etc/apache2/mods-enabled/
 
-for site in /etc/apache2/sites-available/*; do 
-	ln -sf "$site" /etc/apache2/site-enabled
+# Enable sites by creating symlinks in sites-enabled
+for site in /etc/apache2/sites-available/*.conf; do 
+  ln -sf "$site" /etc/apache2/sites-enabled/
+done
 
-for conf in /etc/apache2/conf-availabe/*; do 
-	ln -sf "$conf" /etc/apache2/conf-enabled
-
+# Enable configurations by creating symlinks in conf-enabled
+for conf in /etc/apache2/conf-available/*.conf; do 
+  ln -sf "$conf" /etc/apache2/conf-enabled/
+done
 
 # Start supervisord to manage apache2
 exec supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
-
