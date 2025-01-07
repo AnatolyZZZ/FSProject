2e4df6db626cbb06fd0c670e39a86f1d0d30927a
Anatoly <anatoly@siptrinity.com>
2025-01-07 15:04:41 +0200

    frontend run in container

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -10,6 +10,17 @@ module.exports = {
           configFile: './tsconfig.json',
         }),
       ];
+      // Modify devServer settings
+      if (!webpackConfig.devServer) {
+        webpackConfig.devServer = {};
+      }
+
+      webpackConfig.devServer = {
+        ...webpackConfig.devServer,
+        host: '0.0.0.0', // Ensure it's listening on all interfaces
+        port: 4000, // Ensure the app listens on port 4000
+      };
+
       return webpackConfig;
     },
   },
